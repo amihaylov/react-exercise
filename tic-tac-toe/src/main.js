aa644d1b5b7bf71aff2a06415903f2adbc020d39
Anton Mihaylov <anton.mihaylov7@gmail.com>
2015-12-11 19:01:20 +0200

    Small fixes.

--- tic-tac-toe/src/main.js
+++ tic-tac-toe/src/main.js
@@ -1,5 +1,5 @@
 var React = require('react'),
 	ReactDOM = require('react-dom'),
-	Game = require('components/game');
+	Game = require('./components/game');
 
 ReactDOM.render(<Game size={3} />, document.getElementById('app'));
